7e1aa39b5e812d130f1258e0e5d47a15687250f7
Goudini <luxmain000@gmai.com>
2023-11-26 11:20:26 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Что бы код был понятен и хорошо выглядел, будем придерживаться реализации от интерфейса.
+ * У нас будут классы фигур, которые будут наследоваться от этого интерфейса. Это нужно опять же для удобства кода 
+ * и его расширяемости
+ */
+namespace AreaCalculator
+{
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class Circle : IShape
+    {
+        private double radius;
+
+        public Circle(double radius) => this.radius = radius; // инициализируем конструктор таким образом
+
+
+        public double CalculateArea() => Math.PI* radius * radius; // это наш метод по расчету
+
+    }
+
+    public class Triangle : IShape
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            Func<double> calculateAreaFunc = () =>
+            {
+                double semiPerimeter = (sideA + sideB + sideC) / 2;
+                return Math.Sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+            };
+
+            return calculateAreaFunc();
+            /*
+             * Тут я просто хотел показать что знаю о такой вещи, как делегат. Через лямбду мы просто заворачиваем 
+             * тело метода в делегат Func. 
+             * Разницы с реализацией без делегата толком нет, просто хотел показать технологию.
+             */
+        }
+
+        public bool IsRightTriangle()
+        {
+            double[] sides = { sideA, sideB, sideC };
+            Array.Sort(sides);
+
+            double aSquare = sides[0] * sides[0];
+            double bSquare = sides[1] * sides[1];
+            double cSquare = sides[2] * sides[2];
+
+            return Math.Abs(aSquare + bSquare - cSquare) <= double.Epsilon;
+        }
+    }
+}
---
+++ Task/ShapeTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AreaCalculator.Tests
+{
+    public class ShapeTests
+    {
+        [Fact]
+        public void Circle_CalculateArea_CorrectArea()
+        {
+            // Arrange
+            double radius = 5;
+            double expectedArea = 78.53981633974483;
+            Circle circle = new Circle(radius);
+
+            // Act
+            double actualArea = circle.CalculateArea();
+
+            // Assert
+            Assert.Equal(expectedArea, actualArea);
+        }
+
+        [Fact]
+        public void Triangle_CalculateArea_CorrectArea()
+        {
+            // Arrange
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            double expectedArea = 6;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            // Act
+            double actualArea = triangle.CalculateArea();
+
+            // Assert
+            Assert.Equal(expectedArea, actualArea);
+        }
+
+        [Fact]
+        public void Triangle_IsRightTriangle_RightTriangle()
+        {
+            // Arrange
+            double sideA = 3;
+            double sideB = 4;
+            double sideC = 5;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            // Act
+            bool isRightTriangle = triangle.IsRightTriangle();
+
+            // Assert
+            Assert.True(isRightTriangle);
+        }
+
+        [Fact]
+        public void Triangle_IsRightTriangle_NotRightTriangle()
+        {
+            // Arrange
+            double sideA = 2;
+            double sideB = 3;
+            double sideC = 4;
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+            // Act
+            bool isRightTriangle = triangle.IsRightTriangle();
+
+            // Assert
+            Assert.False(isRightTriangle);
+        }
+    }
+}
